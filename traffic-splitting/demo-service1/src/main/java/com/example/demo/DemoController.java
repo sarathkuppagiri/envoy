4bea3e3f71ebbd002eafb21c4656d9a65e678dec
Sarath kumar Reddy <sarathkumarreddy@Saraths-MacBook-Pro.local>
2021-04-19 10:10:05 -0700

    added traffic splitting example

---
+++ traffic-splitting/demo-service1/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/test")
+	public String sayHello()
+	{
+		return "Result from demo-service-1";
+	}
+
+}
---
+++ traffic-splitting/demo-service3/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/test")
+	public String sayHello()
+	{
+		return "Result from demo-service-3";
+	}
+
+
+}
