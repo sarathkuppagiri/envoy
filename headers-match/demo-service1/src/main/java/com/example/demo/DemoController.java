a28c8bef60dad5ad7709de9f65e83df90ba1df75
Sarath kumar Reddy <sarathkumarreddy@Saraths-MacBook-Pro.local>
2021-04-19 23:12:33 -0700

    added headers match example

---
+++ headers-match/demo-service1/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/listHeaders")
+	public ResponseEntity<?> sayHello(@RequestHeader Map<String, String> headers) {
+		headers.forEach((key, value) -> {
+			System.out.println(String.format("Header '%s' = %s", key, value));
+		});
+		return new ResponseEntity<Map>(headers, HttpStatus.OK);
+
+	}
+
+}
