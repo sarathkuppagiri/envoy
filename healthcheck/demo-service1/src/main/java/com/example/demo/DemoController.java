ebbc46f8c85aae80ce9c5594d3a78da124cc0f48
Sarath kumar Reddy <sarathkumarreddy@Saraths-MacBook-Pro.local>
2021-04-20 12:23:22 -0700

    added healthcheck example

---
+++ healthcheck/demo-service1/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/healthCheck")
+	public ResponseEntity<?> healthCheck() {
+		System.out.println("healthCheck....");
+		return new ResponseEntity("Success", HttpStatus.OK);
+
+	}
+	
+	@GetMapping("/healthCheckFail")
+	public ResponseEntity<?> healthCheckFail() {
+		System.out.println("healthCheckFail....");
+		return new ResponseEntity("Error", HttpStatus.SERVICE_UNAVAILABLE);
+
+	}
+
+}
